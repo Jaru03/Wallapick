85bf4d62726ae64d578776928f666bcb4ed1ea72
Jose Rico <josearudeveloper@gmail.com>
2025-07-26 00:56:05 +0200

    Add: HomePage, ProductsPage, ProductoPage, CartPageand Styles

---
+++ Front-End/src/app/app.config.ts
@@ -0,0 +1,33 @@
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/aura';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { MyPreset } from './utils/myPresetPrimeNg';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimations(),
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(withEventReplay()),
+    provideAnimationsAsync(),
+        providePrimeNG({
+            theme: {
+                preset: MyPreset
+            }
+        })
+  ],
+};
